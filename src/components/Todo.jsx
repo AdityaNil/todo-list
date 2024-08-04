34c9907f5066a257a92248e8b89875bd8540fcba
AdityNil <sirf_aadi@outlook.in>
2024-08-04 23:41:50 +0530

    hi

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -50,7 +50,7 @@ const Todo = () => {
                             </button>
                         </li>
 
-                    })
+                    }) 
                 }
             </ul>
         </section>
